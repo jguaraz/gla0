e71539f2922dec50171a71823b06d18024393432
jguaraz <jguaraz@openmailbox.org>
2016-11-22 08:17:44 -0300

    added highchart functionality

--- gl0/urls.py
+++ gl0/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^g/new/$', views.g_new, name='g_new'),
+    url(r'^chart1$', views.chart1, name='chart1'),
+    #url(r'^chart_data_json/', views.chart_data_json, name = 'chart_data_json'),
+    url(r'^chart2$', views.chart2, name = 'chart2'),
 ]
--- gl0/views.py
+++ gl0/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.utils import timezone
+#from chartit import DataPool, Chart
 from .models import G
 from .forms import GForm
 # Create your views here.
@@ -19,3 +20,56 @@ def g_new(request):
         else:
             form = GForm()
         return render(request, 'gl0/g_edit.html', {'form': form})
+
+'''
+def chart1(request):
+    # Datapool
+    data = DataPool( series=[{'options': {'source': G.objects.all().order_by('datetime') },
+                            'terms': ['datetime','value']}]  )
+    # chart
+    cht = Chart( datasource = data,
+                 series_options = [ {'options':{
+                     type: 'line',
+                     'stacking': False }, 'terms': {'datetime' : ['value']}  }],
+                 chart_options =
+                    { 'title': {'text': 'Control de Glucemia'},
+                     'xAxis': {'title': {'text': 'Fecha/Hora'} } } 
+                 )
+    #Step 3: Send the PivotChart object to the template.
+    return render_to_response({'chart1': cht})
+'''    
+def chart1(request):
+    return render(request, 'gl0/chart1.html')
+
+def chart2(request, chartID = 'chart_ID', chart_type = 'line', chart_height = 500):
+    data = ChartData.check_g_data()
+
+    chart = {"renderTo": chartID, "type": chart_type, "height": chart_height,}  
+    title = {"text": 'Control Glucemia'}
+    xAxis = {"title": {"text": 'Fecha / Hora'}}
+    yAxis = {"title": {"text": 'Data'}}
+    series = [
+        {"name": 'Fecha / Hora', "data": data['datetime']}, 
+        {"name": 'Lectura', "data": data['value']},
+        {"name": 'Promedio movil', "data": data['media']}
+        ]
+
+    return render(request, 'gl0/chart2.html', {'chartID': chartID, 'chart': chart,
+                                                    'series': series, 'title': title, 
+                                                    'xAxis': xAxis, 'yAxis': yAxis})
+
+
+
+class ChartData(object):    
+    def check_g_data():
+        data = {'datetime': [], 'value': [],
+                 'media': []}
+
+        set = G.objects.all()
+
+        for record in set:
+            data['datetime'].append(record.datetime)
+            data['value'].append(record.value)
+            data['media'].append((len(data['value']) < 30) and sum(data['value']) / float(len(data['value'])) or (sum(data['value']) / float(len(data['value']))) )
+
+        return data
